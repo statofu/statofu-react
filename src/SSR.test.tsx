2655b0b5f48148718fc31a34253e0d9eb5b2b244
CgL <licg9999@126.com>
2023-06-26 16:38:40 +0800

    test: verify SSR and refine the rest (#2)

---
+++ src/SSR.test.tsx
@@ -0,0 +1,54 @@
+/**
+ * @jest-environment node
+ */
+import { JSDOM } from 'jsdom';
+import React, { FC } from 'react';
+import { renderToString } from 'react-dom/server';
+
+import { useOperate } from './Operate';
+import { useSnapshot } from './Snapshot';
+import { StoreProvider } from './Store';
+
+interface A {
+  a: string;
+}
+const $a: A = { a: 'a' };
+
+test(`gets SSR'd with no error`, () => {
+  const ResultText: FC = () => {
+    const a = useSnapshot($a);
+    return <div role="result-text">{a.a}</div>;
+  };
+
+  const Action: FC = () => {
+    const operateA = useOperate($a);
+    return (
+      <button
+        onClick={() => {
+          operateA((a) => ({ ...a, a: `${a.a}+` }));
+        }}
+      >
+        Click
+      </button>
+    );
+  };
+
+  expect(() => {
+    const a1: A = { a: 'a+' };
+
+    const jsdom = new JSDOM(
+      renderToString(
+        <StoreProvider
+          onCreate={(store) => {
+            store.operate($a, a1);
+          }}
+        >
+          <ResultText />
+          <Action />
+        </StoreProvider>
+      )
+    );
+
+    expect(jsdom.window.document.querySelector('[role="result-text"]')).toHaveTextContent(a1.a);
+  }).not.toThrow();
+});
